e65211431c4a7322c37f135a6a9e2ea8b1fd0251
Peter Bui <pbui@nd.edu>
2023-01-09 20:52:47 -0500

    scripts: update dump_tas

--- scripts/dump_tas.py
+++ scripts/dump_tas.py
@@ -3,4 +3,5 @@
 import yaml
 
 for index, ta in enumerate(yaml.safe_load(open('static/yaml/tas.yaml')), 1):
-    print(f'{index}. {ta["name"]} ({ta["netid"]}@nd.edu)')
+    #print(f'{index}. {ta["name"]} {ta["netid"]}@nd.edu {ta["github"]}')
+    print(f'{ta["netid"]}')
